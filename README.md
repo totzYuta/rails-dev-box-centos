309cba436b5b61c230b80e2b0b2ed58b61b43591
totz <yuta.totz@gmail.com>
2015-04-26 05:45:30 +0900

    Add bootstrap for building rails development environment

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Vagrant Box of CentOS for Rails Development
 ## !DRAFT!
+
+- Ruby 2.2.2
+- Rails ~>4.2.0
+- CentOS 6.5
+- MySQL
+
+*please use bash
---
+++ bootstrap.sh
@@ -0,0 +1,39 @@
+echo 'Installing git...'
+sudo yum install git
+
+echo 'Installing packages...'
+sudo yum install gcc-c++ glibc-headers openssl-devel readline libyaml-devel readline-devel zlib zlib-devel
+sudo yum install nodejs
+
+echo 'Installing rbenv...'
+sudo git clone https://github.com/sstephenson/rbenv.git /usr/local/rbenv 
+sudo echo 'export RBENV_ROOT="/usr/local/rbenv"' >> ~/.bash_profile
+sudo echo 'export PATH="${RBENV_ROOT}/bin:${PATH}"' >> ~/.bash_profile
+sudo echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+source ~/.bash_profile
+
+echo 'Installing ruby-build...'
+sudo git clone https://github.com/sstephenson/ruby-build.git /usr/local/rbenv/plugins/ruby-build
+
+echo 'Installing Ruby...'
+sudo chmod -R 777 /usr/local/rbenv/
+rbenv install -v 2.2.2
+rbenv rehash
+rbenv global 2.2.2
+source ~/.bash_profile
+
+echo 'Installing Rails...'
+gem update --system
+sudo yum -y install libxml2 libxslt libxml2-devel libxsl
+gem install nokogiri -v 1.6.2.1 -- --use-system-librarie
+gem install --no-ri --no-rdoc rails --version "~>4.2.0"
+gem install bundler
+rbenv rehash
+
+echo 'Intalling MySQL'
+sudo yum -y install mysql-server
+sudo yum install mysql-devel
+echo 'Starting mysqld'
+sudo service mysqld start
+
+echo 'All Done! Enjoy Your Rails Development.'
